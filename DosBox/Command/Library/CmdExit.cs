835f256e46d2c531de93d23ba6ea54c70ad63eee
Nathan <nathan.mccarthy@quanitum.com.au>
2016-10-25 11:57:30 +1100

    init with exit

---
+++ DosBox/Command/Library/CmdExit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DosBox.Command.Framework;
+using DosBox.Interfaces;
+
+namespace DosBox.Command.Library
+{
+    public class CmdExit : DosCommand
+    {
+        public CmdExit(string cmdName, IDrive drive) : base(cmdName, drive)
+        {
+        }
+
+        public override void Execute(IOutputter outputter)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
